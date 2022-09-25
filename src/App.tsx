4367cdd9a5abca842e86e2632e29e01dcfb4c99e
VictorYarosh <77171768+VictorYarosh@users.noreply.github.com>
2022-09-25 13:09:51 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-
-
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -28,7 +23,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
